eec98387df1245adfd970252592a2e009422feae
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-19 10:40:13 -0600

    Update the code with a vue logic way to do it

--- src/components/ToastNotification.vue
+++ src/components/ToastNotification.vue
@@ -1,43 +1,43 @@
 <template>
-  <div>
-    <div v-if="hide_message">
-         <h2 ref="message"></h2>
+  <div v-if="notifications.length > 0">
+    <div v-for="(notification, index) in notifications" :key="index">
+         <h3>{{ notification }}</h3>
+         <button @click="dismiss_message_function(index, $event)">
+        Dismiss Message
+      </button>
     </div>
    
     <div v-if="dismiss_message">
-      <button @click="dismiss_message_function" ref="dismiss_button">
-        Dismiss Message
-      </button>
+      
     </div>
   </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            notifications: []
+        }
+    },
   methods: {
-    handle_delete(message) {
-      this.$refs[`message`].insertAdjacentHTML(`beforeend`, `${message}`);
-
-      this.dismiss_message = !this.dismiss_message;
+    notify_user(message) {
+      if(this.notifications.length >= 3){
+        this.notifications.splice(0,1)
+      }
+      this.notifications.push(message)
     },
 
-    dismiss_message_function() {
-        this.hide_message = !this.hide_message;
-        this.dismiss_message = !this.dismiss_message;
+    dismiss_message_function(index) {
+        this.notifications.splice(index, 1)
     }
     ,
   },
 
   mounted() {
-    this.$root.$on(`toast_notify`, this.handle_delete);
+    this.$root.$on(`toast_notify`, this.notify_user);
   },
 
-  data() {
-    return {
-      dismiss_message: false,
-      hide_message: true
-    };
-  },
 };
 </script>
 
